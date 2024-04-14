083a90b926efac4052cddc1fd5c6deff958d04cd
AbhishekIITK-Y22 <136859559+AbhishekIITK-Y22@users.noreply.github.com>
2024-04-14 11:28:26 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,25 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-
-df = pd.read_csv('train.csv')
-df_test = pd.read_csv('test.csv')
+data_train = pd.read_csv('train.csv')
+data_test = pd.read_csv('test.csv')
 
 features = ['Constituency ∇', 'Party', 'Criminal Case', 'Total Assets', 'Liabilities', 'state']
 target = 'Education'
 
-combined = pd.concat([df[features], df_test[features]])
+combined_data = pd.concat([data_train[features], data_test[features]])
 
-combined = pd.get_dummies(combined)
+combined_data = pd.get_dummies(combined_data)
 
-X_train = combined[:len(df)]
-X_test = combined[len(df):]
+X_train = combined_data[:len(data_train)]
+X_test = combined_data[len(data_train):]
 
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+random_forest_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 
-y_train = df[target]
-rf_classifier.fit(X_train, y_train)
+y_train = data_train[target]
+random_forest_classifier.fit(X_train, y_train)
 
-predictions = rf_classifier.predict(X_test)
+predictions = random_forest_classifier.predict(X_test)
 
-submission_df = pd.DataFrame({'ID': df_test['ID'], 'Education': predictions})
-submission_df.to_csv('submission_random_forest.csv', index=False)
+submission_data = pd.DataFrame({'ID': data_test['ID'], 'Education': predictions})
+submission_data.to_csv('submission_random_forest.csv', index=False)
